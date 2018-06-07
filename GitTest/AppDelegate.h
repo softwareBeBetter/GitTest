80abdd34655271f9f6ce686283cb6a69afb61e67
mR yang <you@example.com>
2018-06-07 18:09:41 +0800

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by mR yang on 2018/6/7.
+//  Copyright © 2018年 mR yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by mR yang on 2018/6/7.
+//  Copyright © 2018年 mR yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
