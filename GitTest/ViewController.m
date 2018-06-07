77e13fc1dc52f8c6b252fce9109502b7a4b9e279
mR yang <you@example.com>
2018-06-07 18:59:23 +0800

    finished

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -19,7 +19,7 @@
   // Do any additional setup after loading the view, typically from a nib.
 }
 -(IBAction)test:(id)sender{
-  NSLog(@"this is test");
+  NSLog(@"finished");
 }
 
 - (void)didReceiveMemoryWarning {
